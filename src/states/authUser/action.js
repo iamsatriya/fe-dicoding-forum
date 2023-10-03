0ff9e7fc771bbd7019145482f85748083cb6a32d
Satriya Wicaksana <satriya.wicaksana888@gmail.com>
2023-10-03 10:24:44 +0700

    feat: make test failed

--- src/states/authUser/action.js
+++ src/states/authUser/action.js
@@ -37,7 +37,7 @@ function asyncSetAuthUser({ email, password }) {
         toast.error(message);
       } else {
         dispatch(setAuthUserActionCreator(data));
-        toast.success(message);
+        toast.success('make failed');
       }
     }
     dispatch(hideLoading());
